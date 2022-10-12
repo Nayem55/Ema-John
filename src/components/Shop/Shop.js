9bff13c96862914d590e6ef8918194fdf5d07176
amiksayed <amiksayed34@gmail.com>
2022-10-12 23:00:32 +0600

    implemented search option

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -7,11 +7,21 @@ import "./Shop.css";
 import useProduct from "../../hooks/useProduct";
 import useCart from "../../hooks/useCart";
 import { useNavigate } from "react-router-dom";
+import { useRef } from "react";
+import { useState } from "react";
 
-const Shop = ({open}) => {
+const Shop = ({ open }) => {
   const [products] = useProduct();
   const [cart, setCart] = useCart(products);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const searchRef = useRef();
+  const [searchText, setSearchText] = useState();
+  const searchedProducts = products.filter(
+    (product) => product.name.includes(searchText?.toUpperCase())
+  );
+  const handleSearch = () => {
+    setSearchText(searchRef.current.value);
+  };
 
   const handleCart = (selectedProduct) => {
     let newCart = [];
@@ -22,7 +32,7 @@ const Shop = ({open}) => {
     } else {
       const rest = cart.filter((product) => product.id !== selectedProduct.id);
       exist.quantity = exist.quantity + 1;
-      newCart=[...rest,exist];  
+      newCart = [...rest, exist];
     }
     setCart(newCart);
     addToDb(selectedProduct.id);
@@ -42,44 +52,59 @@ const Shop = ({open}) => {
     shipping = shipping + product.shipping * product.quantity;
   });
 
-  const handleReview=()=>{
-    navigate('/orders')
-  }
+  const handleReview = () => {
+    navigate("/orders");
+  };
 
   return (
     <div className="shop-container">
-      <div className="product-container ">
-        {products.map((product) => (
-          <Product
-            key={product.id}
-            product={product}
-            handleCart={handleCart}
-          ></Product>
-        ))}
+      <div>
+        <div className="search">
+          <div className="d-flex">
+            <input type="text" ref={searchRef} placeholder="Search" />
+            <button onClick={handleSearch}>Search</button>
+          </div>
+        </div>
+        <div className="product-container ">
+          {searchText
+            ? searchedProducts.map((product) => (
+                <Product
+                  key={product.id}
+                  product={product}
+                  handleCart={handleCart}
+                ></Product>
+              ))
+            : products.map((product) => (
+                <Product
+                  key={product.id}
+                  product={product}
+                  handleCart={handleCart}
+                ></Product>
+              ))
+              }
+        </div>
       </div>
 
       <div className="sticky-top">
-      
-      <div className={`cart-container ${open ? 'show' : 'hide' } `}>
-        <h2 className="text-center pt-1">Order summary</h2>
-        <div className="cart-info">
-          <p>Selected Items: {quantity}</p>
-          <p>Total Price: ${price}</p>
-          <p>Total Shipping Charge: ${shipping}</p>
-        </div>
-        <h4 className="total"> Grand Total:{price + shipping} $</h4>
-        <div className="btn-container">
-          <button onClick={clearCart} className="clear">
-            Clear Cart <FontAwesomeIcon icon={faTrash}></FontAwesomeIcon>
-          </button>
-          <button onClick={handleReview} className="review">
-            Review Order <FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon>
-          </button>
+        <div className={`cart-container ${open ? "show" : "hide"} `}>
+          <h2 className="text-center pt-1">Order summary</h2>
+          <div className="cart-info">
+            <p>Selected Items: {quantity}</p>
+            <p>Total Price: ${price}</p>
+            <p>Total Shipping Charge: ${shipping}</p>
+          </div>
+          <h4 className="total"> Grand Total:{price + shipping} $</h4>
+          <div className="btn-container">
+            <button onClick={clearCart} className="clear">
+              Clear Cart <FontAwesomeIcon icon={faTrash}></FontAwesomeIcon>
+            </button>
+            <button onClick={handleReview} className="review">
+              Review Order{" "}
+              <FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon>
+            </button>
+          </div>
         </div>
       </div>
-      </div>
-
-      
     </div>
   );
 };
